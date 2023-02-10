d499b2fe307e9cfddf9c5aac0b05006c43810819
achraf-developer <technichome.achraf@gmail.com>
2023-02-10 15:44:15 +0200

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    $(".hero-slider").slick({
+      autoplay:true,
+      autoplaySpeed: 3000,
+      accessibility:true,
+      arrows: true,
+      fade: true,
+      dots:false,
+      cssEase: "ease",
+      easing:'linear',
+      draggable:false,
+      slidesToShow: 1,
+      //     adaptiveHeight: true,
+    });
+  });
+  
